5868da54f0cd91a75ae94ad2b60ac3081cbbe75a
LAPTOP-junto <juntou1230@gmail.com>
2023-04-25 16:46:22 +0900

    update: output.jsがないことによるエラー対策

--- src/init.js
+++ src/init.js
@@ -8,6 +8,8 @@ let input = fs.readFileSync('main/input.txt', 'utf-8').split("\\n");
 
 fs.writeFileSync('main/script.js',  OutputText);
 fs.writeFileSync('main/input.txt',  '');
+try {
 fs.unlinkSync('main/output.js');
+} catch {}
 
 console.log("初期化しました");
