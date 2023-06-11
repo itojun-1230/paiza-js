6b088bb87ba6995643f82c52b5ab6609619a1c68
DESKTOP-junto <juntou1230@gmail.com>
2023-06-11 17:12:40 +0900

    update: ソートアルゴリズム変更

--- main/script.js
+++ main/script.js
@@ -1,60 +1,55 @@
-const SplitNumber = ( num ) => {    //引数を" "で分割してNumberにして返す
-    
-    return num.split(" ").map(Number);
+const SplitNumber = (num) => {    //引数を" "で分割してNumberにして返す
+
+  return num.split(" ").map(Number);
 }
-const DeepCopy = ( e ) => {    //引数をDeepCopyして返す
-    
-    return JSON.parse(JSON.stringify(e));
+const DeepCopy = (e) => {    //引数をDeepCopyして返す
+
+  return JSON.parse(JSON.stringify(e));
 }
-const ShellSort = (arr, sort = true) => {  //数列をシェルソートする sort = true 昇順 sort = false 降順
-    const Arr = DeepCopy(arr);  //DeepCopy
-    
-    let h = 0;
-    while( h <= Arr.length / 9 ) {  //間隔計算
-        h = 3 * h + 1;
+const QuickSort = (arr, sort = 0) => {  //数列をクイックソートする 昇順 sort = 0 降順 sort = 1
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let Pivot = arr[Math.floor(Math.random() * arr.length)];
+  const Left = [], Mid = [], Right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (sort == 0 ? arr[i] < Pivot : arr[i] > Pivot) {
+      Left.push(arr[i]);
+    } else if (sort == 0 ? arr[i] > Pivot : arr[i] < Pivot) {
+      Right.push(arr[i]);
+    } else {
+      Mid.push(arr[i]);
     }
-    while(h > 0){
-        for(let i = h; i < Arr.length; i++){
-            const tmp = Arr[i];
-            
-            let index = i - h;
-            while(index >= 0 && sort ? Arr[index] > tmp : Arr[index] < tmp){
-                Arr[index + h] = Arr[index];
-                index -= h;
-            }
-            Arr[index + h] = tmp;
-        }
+  }
 
-        h = Math.floor(h / 3);
+  return [...QuickSort(Left, sort), ...Mid, ...QuickSort(Right, sort)];
+}
+const BinarySearch = (arr, data) => { //引数arrのdataのindexを返す二分探索関数 見つからなかった場合-1を返す
+  let max = arr.length;
+  let min = 0;
+
+  let res = -1;
+  while (max >= min) {
+    const Mid = Math.floor((max + min) / 2);
+
+    if (arr[Mid] < data) {
+
+      min = Mid + 1;
+    } else if (arr[Mid] > data) {
+
+      max = Mid - 1;
+    } else if (arr[Mid] == data) {
+      res = Mid;
+      break;
     }
-
-    return Arr;
-}
-const BinarySearch = ( arr, data ) => { //引数arrのdataのindexを返す二分探索関数 見つからなかった場合-1を返す
-    let max = arr.length;
-    let min = 0;
-
-    let res = -1;
-    while(max >= min) {
-        const Mid = Math.floor(( max + min ) / 2);
-
-        if( arr[Mid] < data ) {
-
-            min = Mid + 1;
-        }else if(arr[Mid] > data) {
-
-            max = Mid - 1;
-        }else if(arr[Mid] == data){
-            res = Mid;
-            break;
-        }
-    }
-    return res;
+  }
+  return res;
 }
 
-const Main = ( input ) => {
+const Main = (input) => {
 
 
 }
 
-Main( require("fs").readFileSync("main/input.txt", "utf8").split("\n") );
+Main(require("fs").readFileSync("main/input.txt", "utf8").split("\n"));
